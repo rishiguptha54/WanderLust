7fa48bb71415b37cc72f4c60393c6a0d4f360ad1
Rishi Guptha <rishi2002guptha@gmail.com>
2025-02-23 00:56:28 +0530

    styling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listings");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,7 +23,9 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -33,6 +37,13 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index.ejs", { allListings });
 });
 
+
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
 //show route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
@@ -40,10 +51,33 @@ app.get("/listings/:id", async (req, res) => {
     res.render("listings/show.ejs", { listing });
 });
 
+//create route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
 
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
 
+//update route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, req.body.listing);
+    res.redirect(`/listings/${id}`);
+});
 
-
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
 // app.get("/testListings", async (req, res) => {
 //     let sampleListing = new Listing({
 //     title: "My New Villa",
