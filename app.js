4256be71c2cf34d1cf51618d3850ed6b78e41413
Rishi Guptha <rishi2002guptha@gmail.com>
2025-02-25 10:47:32 +0530

    auth

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const { listingSchema, reviewSchema } = require("./schema");
-const listings = require("./routes/listing");
-const reviews = require("./routes/reviews");
+const listingsRouter = require("./routes/listing");
+const reviewsRouter = require("./routes/reviews");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const userRouter = require("./routes/user");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -51,14 +55,33 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
-})
+});
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "delta-student"
+    });
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
+});
+
+
+app.use("/listings",listingsRouter);
+app.use("/listings/:id/reviews",reviewsRouter);
+app.use("/",userRouter);
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
